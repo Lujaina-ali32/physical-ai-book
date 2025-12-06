3489a0f39c7570997cc60f1cd6c96a6e44ede5d3
Lujaina-ali32 <lujainaali10@gmail.com>
2025-12-07 03:54:56 +0500

    Updates

--- assets/js/acecf23e.70c7f749.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkhumanoid_book=globalThis.webpackChunkhumanoid_book||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
---
+++ assets/js/acecf23e.e2770cb6.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkhumanoid_book=globalThis.webpackChunkhumanoid_book||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/physical-ai-book/blog","blogTitle":"Blog","authorsListPath":"/physical-ai-book/blog/authors"}')}}]);
